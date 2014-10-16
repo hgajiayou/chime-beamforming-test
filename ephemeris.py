102f98802af8ed3941654b7c2d3df8d2f04654b3
Nolan Denman <denman@astro.utoronto.ca>
2014-10-15 21:12:02 -0400

    First working version of the ephem code

--- ephemeris.py
+++ ephemeris.py
@@ -1,10 +1,9 @@
-### Ephemeris double-checker for CHIME ###
+### Ephemeris double-checker for CHIME
 # NTD
 # input: Source RA and DEC and the exact time to begin simulating
 # output: the alt and az of the object
-    # may include time-evolution and therefore need a timestamp.
 
-### Import Statements
+### Import statements
 import sys
 import numpy as np
 from datetime import *
@@ -19,23 +18,29 @@ CHI_lat  = 45
 CHI_long = -120
 ########################
 
-### Parameters of the output - move to insertion via CLI later on
+### Parameters of the sim
 delta_t = 1.e-2 #The time resolution of the output, in decimal hours
 sim_time = 1 #Total time to simulate after the supplied start time, in hours
-
-if len(sys.argv) != 4:
-    print "Usage: python ephemeris.py SOURCE_RA SOURCE_DEC TIME_[YYYY][MM][DD]T[HH][MM][SS]Z"
 
 # We assume: RA in decimal hours, DEC in decimal degrees. May convert if needed
 
 ### Conversions involving ISO-formatted UTC:
 # 2013 11 18 T 01 41 27 Z
 # YYYY MM DD   HH MM SS
+# Time format: [YYYY][MM][DD]T[HH][MM][SS]Z
+
+# python ephemeris.py 0 0 20131118T014127Z 0.05 24 | less
+
+if len(sys.argv) != 6:
+    print "Usage: python ephemeris.py SOURCE_RA SOURCE_DEC TIME DELTA_T SIM_LENGTH"
 
 else:
     source_ra  = float(sys.argv[1])
     source_dec = float(sys.argv[2])
     time_start = sys.argv[3]
+    delta_t    = float(sys.argv[4])
+    sim_time   = float(sys.argv[5])
+
 
     # setting the time constants
     time_J2000_epoch = datetime(2000, 01, 01, 11, 58, 55, 816000, tzinfo=tzutc()) 
@@ -45,19 +50,33 @@ else:
     time_current = datetime(int(time_start[:4]),int(time_start[4:6]),int(time_start[6:8]),int(time_start[9:11]),int(time_start[11:13]),int(time_start[13:15]),0,tzinfo=tzutc())
 
     for i in np.linspace(0, sim_time, sim_time/delta_t):
+        days_since_J2000 = time_current - time_J2000_epoch
+        decimal_days_since_J2000 = days_since_J2000.days + days_since_J2000.seconds/(24*3600.)
 
-        alt = 0
-        az  = 0
+        local_siderial_time = 100.46 + 0.985647*decimal_days_since_J2000 + CHI_long + 15*(time_current.hour+time_current.minute/60.+time_current.second/3600.)
+        while(local_siderial_time < 0):
+            local_siderial_time += 360.
+        while(local_siderial_time > 360):
+            local_siderial_time -= 360.
 
-        #Get days since the J2000.0 epoch //(UT - offset)/some factor?
-        #Calculate LST from base+_d_+UT, then % to [0,360] in degrees
-        #Calc. hour angle = LST-RA, then % to [0,360] in degrees (only concern is <0)
-        # Then, nasty trig:
-            #sin(ALT) = sin(DEC)*sin(LAT)+cos(DEC)*cos(LAT)*cos(HA)
-            #cos(AZ) = (sin(DEC) - sin(ALT)*sin(LAT))/(cos(ALT)*cos(LAT))
-        # Reverse trig, output.
+        hour_angle = local_siderial_time - source_ra
+        if(hour_angle < 0):
+            hour_angle += 360.
+
+        alt = np.sin(source_dec*np.pi/180.)*np.sin(CHI_lat*np.pi/180.)+np.cos(source_dec*np.pi/180.)*np.cos(CHI_lat*np.pi/180.)*np.cos(hour_angle*np.pi/180.)
+        alt = np.arcsin(alt)*180/np.pi
+
+        if(np.cos(alt*np.pi/180.) != 0 and np.cos(CHI_lat*np.pi/180.) != 0):
+            az = (np.sin(source_dec*np.pi/180.) - np.sin(alt*np.pi/180.)*np.sin(CHI_lat*np.pi/180.))/(np.cos(alt*np.pi/180.)*np.cos(CHI_lat*np.pi/180.))
+            az = np.arccos(az)*180/np.pi
+        else:
+            az = NaN
+
+        if(np.sin(hour_angle*np.pi/180.) >= 0):
+            az = 360 - az
 
         tc=time_current
-        print str(tc.year).zfill(4)+str(tc.month).zfill(2)+str(tc.day).zfill(2)+"T"+str(tc.hour).zfill(2)+str(tc.minute).zfill(2)+str(tc.second).zfill(2)+"Z,"+ str(i) +","+ str(alt) +","+ str(az)
+        sep = "\t"
+        print str(tc.year).zfill(4)+str(tc.month).zfill(2)+str(tc.day).zfill(2)+"T"+str(tc.hour).zfill(2)+str(tc.minute).zfill(2)+str(tc.second).zfill(2)+"Z"+sep+ str(i) +sep+ str(alt) +sep+ str(az)
 
         time_current += time_per_step
