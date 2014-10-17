8d3bf6c5f82a0dc8e920d7aa19c29b3b824893ed
Nolan Denman <denman@astro.utoronto.ca>
2014-10-17 14:37:52 -0400

    First mostly-working version of the delay code

--- delay_calculator.py
+++ delay_calculator.py
@@ -1,21 +1,29 @@
 ### CHIME Feed-Delay Calculator
 # NTD
-# input: source alt and az, feed layout and cable delays
-# output: grid of the delays required to beamform
+# input: source alt and az, feed layout and cable delay files
+# output: list of the delays required to beamform
 
 ### Import statements
 import sys
 import numpy as np
 
 ### Constants 
+
 # CHIME rotation from celestial north - in radians, clockwise from above
 chime_offset_clockwise = 0*(np.pi/180.)
+
 # inverse of speed of light - in nanoseconds per meter
 one_over_c = 3.3356
+
 # Location of file containing feed positions in [x]\t[y]\n format, in m
 feed_config_file = "/home/qingtang/pulsar/feed_loc_layout27.txt"
+
 # Location of file containing cable delays in [t]\n format, in ns
 cable_delay_file = "/home/denman/chime_proc/chime_cable_delays.dat"
+
+### TEST CHANGES ###
+feed_config_file = "test_grid.dat"
+####################
 
 if len(sys.argv) != 3:
     print "Usage: python delay_calculator.py SOURCE_ALT SOURCE_AZ"
@@ -25,24 +33,20 @@ else:
     source_az  = float(sys.argv[2])*np.pi/180.
     # assuming input in degrees, convert to radians for ease of use
 
-    # calculate the east-west and north-south angles (from zenith)
-    geom_delay_east  = (0.5*np.pi - source_alt)*np.sin(source_az+chime_offset_clockwise)
-    geom_delay_north = (0.5*np.pi - source_alt)*np.cos(source_az+chime_offset_clockwise)
+    alt_correction_term = np.cos(source_alt)
 
     # read feed data from appropriate config files
     feed_positions = np.loadtxt(feed_config_file)
     cable_delays = np.loadtxt(cable_delay_file)
+    feed_delays = []
 
     #NOTE: We assume here that all files list the feeds in the same order
 
-    # Calculate projected seperations for each feed
-    #   Convert to a delay in time (microsec? nanosec?) 
-
-    feed_delays = []
-
     for i in range(len(feed_positions)):
-        temp_ew = feed_positions[i][0]*np.sin(geom_delay_east)
-        temp_ns = feed_positions[i][1]*np.sin(geom_delay_north)
-        feed_delays.append(np.sqrt(temp_ew*temp_ew + temp_ns*temp_ns)*one_over_c + cable_delays[i])
+        projection_angle = np.arctan2(feed_positions[i][1],feed_positions[i][0])
+        offset_distance  = alt_correction_term*np.sqrt(feed_positions[i][0]*feed_positions[i][0] + feed_positions[i][1]*feed_positions[i][1])
+        effective_angle  = projection_angle - (source_az - chime_offset_clockwise)
 
-    print feed_delays
+        feed_delays.append(np.sin(effective_angle)*offset_distance*one_over_c + cable_delays[i]) 
+
+print feed_delays
