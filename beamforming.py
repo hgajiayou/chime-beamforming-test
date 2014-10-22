86fb0a6913f818d5c13b0eba4712520d6a1452b9
Nolan Denman <denman@astro.utoronto.ca>
2014-10-22 12:00:51 -0400

    Finished first working (?) version of beamformer

--- beamforming.py
+++ beamforming.py
@@ -1,11 +1,15 @@
 ### CHIME Test Beamformer
 # NTD
 # input: list of the delays required to beamform, baseband data file
-# output: visibilities from correlation of delayed timestreams
+# output: sum of phase-delayed timestreams
+
+
 
 ### Import statements
 import numpy as np
 import sys
+
+
 
 ### Constants 
 
@@ -16,26 +20,29 @@ delay_file = "example_delays.dat"
 baseband_data_file = "/mnt/CHIMEdata/baseband_data/B0329/1000.dat"
 
 # save location for output files
-output_prefix = "./corr_data/correlated-visibility-"
-output_suffix = ".dat"
+output_file = "./corr_data/beamformed-result.dat"
 
 # constants related to file and packet structure
 header_length_bytes = 58
-data_size_bytes     = 8192
 packet_size_bytes   = 9296
-footer_size_bytes   = packet_size_bytes - (data_size_bytes + header_length_bytes)
 frames_per_packet   = 4
 frame_length        = 2048
 n_elements          = 16
 n_frequencies       = 128
 packets_in_file     = 64*1024
 
-# length of the timestep for each frame, in nanosec
-timestep_in_ns = 1024*(1/800.E6)*1.E9
+# frequency information
+max_freqency_ghz = 0.8
+min_freqency_ghz = 0.4
+frequency_array = np.linspace(min_freqency_ghz, max_freqency_ghz, n_frequencies)
+    # if frequencies reversed, swap start/stop
+
 
 ### TEST CHANGES ###
 packets_in_file = 10
 ####################
+
+
 
 ### Main Program
 if len(sys.argv) != 1:
@@ -46,14 +53,14 @@ delay_times = np.loadtxt(delay_file)
 # ensure that units are consistent - currently in nanoseconds
 
 raw_baseband_data = []
-# shape of data cube -> (timestep, element, frequency)
+# shape of data cube is (timestep, element, frequency)
 
 data_file = open(baseband_data_file, "rb")
 
 # read in data from the baseband packet files
 for i in range(packets_in_file):
     data_file.seek(i*packet_size_bytes + header_length_bytes, 0)
-    for j in range(frames_per_packet):
+    for k in range(frames_per_packet):
         read_data = np.fromstring(data_file.read(frame_length),dtype=np.uint8)
         data_real = ((read_data >> 4).astype(np.int32) - 8)
         data_imag = ((read_data & 0xf).astype(np.int32) - 8)
@@ -62,15 +69,18 @@ for i in range(packets_in_file):
         data_complex = data_complex.reshape((n_elements, n_frequencies), order='F')
         raw_baseband_data.append(data_complex)
 
-print len(raw_baseband_data)
-print len(raw_baseband_data[0])
-print len(raw_baseband_data[0][0])
+delay_phases = delay_times*2*np.pi
+# to convert to true delay, multiply by (frequency / 1GHz) - so 0.4 through 0.8
 
-#print raw_baseband_data
+beamformed_data = []
 
-# apply the delays in terms of time-steps (units of 1/800MHz)
-#   interpolate? round? do some n-step function?
+for i in range(packets_in_file*frames_per_packet): #for each timestep
+    beamformed_data.append([])
+    for k in range(n_frequencies): #for each frequency
+        summed_beam = 0.
+        for l in range(n_elements): #for each element
+            phase=-1.0j*delay_phases[l]*frequency_array[k]
+            summed_beam +=  raw_baseband_data[i][l][k]*np.exp(phase)
+        beamformed_data[i].append(summed_beam)
 
-# correlate the delayed timestreams to generate visibilities
-
-# output "[r]\t[i]\t[mag]\t[phase]" for each visibility
+np.savetxt(output_file, beamformed_data, fmt = '%s\t'*n_frequencies)
